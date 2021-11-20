e327ab6edd2b5fda04a445a95c210b862b48749f
Simo Siren <sirensimo7@gmail.com>
2021-11-20 18:49:30 +0200

    3pv

---
+++ WebAppEkaS2019/Controllers/HomeController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Web.Mvc;
+using WebAppEkaS2019.Models;
+
+namespace WebAppEkaS2019.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            ViewBag.MessageModal= "Tietoja Northwind-yhtiöstä ja Careeriasta";
+            ViewBag.LoginError = 0; //Ei virhettä...
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Tietoja Northwind-yhtiöstä ja Careeriasta";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Yhteystiedot";
+
+            return View();
+        }
+
+        public ActionResult Laskuri()
+        {
+            string ilmoitus = "";
+            int i = 0;
+            string connStr = "Server=L17HE01A;Database=NorthwindLargeDev;Trusted_Connection=False;User Id=sa;Password=Careeria2019;"; //<--oman koneen SQL Serverin sekä tietokannan nimi
+            SqlConnection conn = new SqlConnection(connStr);
+            conn.Open();
+
+            string sql = "Select * from customers where country = 'Finland'";
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                i++;
+                string otsikko0 = reader.GetName(0);
+                string kentta0 = reader.GetString(0);
+                string kentta1 = reader.GetString(1);
+                string kentta2 = reader.GetString(2);
+                ilmoitus = String.Format("Rivi{4}: {0} = {1}  {2}  {3}", otsikko0, kentta0, kentta1, kentta2, i.ToString());
+            }
+            reader.Close();
+            cmd.Dispose();
+            conn.Dispose();
+
+            string apumuuttuja="";
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    apumuuttuja = apumuuttuja + "Laskurin arvo on nyt = " + i.ToString();
+            //}
+
+            ViewBag.Message = ilmoitus;
+            return View();
+        }
+
+        public ActionResult Map()
+        {
+            ViewBag.Message = "Saapumisohje";
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Authorize(Logins LoginModel)
+        {
+            northwindEntities db = new northwindEntities();
+            //Haetaan käyttäjän/Loginin tiedot annetuilla tunnustiedoilla tietokannasta LINQ -kyselyllä
+            var LoggedUser = db.Logins.SingleOrDefault(x => x.UserName == LoginModel.UserName && x.PassWord == LoginModel.PassWord);
+            if (LoggedUser != null)
+            {
+                ViewBag.LoginMessage = "Successfull login";
+                ViewBag.LoggedStatus = "In";
+                ViewBag.LoginError = 0; //Ei virhettä...
+                Session["UserName"] = LoggedUser.UserName;
+                Session["LoginID"] = LoggedUser.LoginId;
+                //Session["AccessLevel"] = LoggedUser.AccessLevel;
+                return RedirectToAction("Index", "Home"); //Tässä määritellään mihin onnistunut kirjautuminen johtaa --> Home/Index
+            }
+            else
+            {
+                ViewBag.LoginMessage = "Login unsuccessfull";
+                ViewBag.LoggedStatus = "Out";
+                ViewBag.LoginError = 1; //Pakotetaan modaali login-ruutu uudelleen koska kirjautumisyritys on epäonnistunut
+                LoginModel.LoginErrorMessage = "Tuntematon käyttäjätunnus tai salasana.";
+                //return View("Login", LoginModel);
+                return View("Index", LoginModel);
+            }
+        }
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            ViewBag.LoggedStatus = "Out";
+            return RedirectToAction("Index", "Home"); //Uloskirjautumisen jälkeen pääsivulle
+        }
+
+        public ActionResult MustAuthorize()
+        {
+            ViewBag.LoginError = 1;
+            return View("Index"); //pääsivulle autentikontia pyytämään
+        }
+    }
+
+
+
+
+}
---
+++ WebAppEkaS2019/Controllers/ProductsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAppEkaS2019.Models;
+using System.Web.Mvc;
+using PagedList;
+
+namespace WebAppEkaS2019.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index(string sortOrder, string currentFilter1, string searchString1, string ProductCategory, string currentProductCategory, int? page, int? pagesize)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.ProductNameSortParm = String.IsNullOrEmpty(sortOrder) ? "productname_desc" : "";
+            ViewBag.UnitPriceSortParm = sortOrder == "UnitPrice" ? "UnitPrice_desc" : "UnitPrice";
+            //Hakufiltterin laitto muistiin
+            if (searchString1 != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString1 = currentFilter1;
+            }
+
+            ViewBag.currentFilter1 = searchString1;
+
+            //Tuotecategoriahakufiltterin laitto muistiin
+            if ((ProductCategory != null) && (ProductCategory != "0"))
+            {
+                page = 1;
+            }
+            else
+            {
+                ProductCategory = currentProductCategory;
+            }
+
+            ViewBag.currentProductCategory = ProductCategory;
+
+            northwindEntities db = new northwindEntities();
+
+            var tuotteet = from p in db.Products
+                           select p;
+
+            //Tehdään tässä kohden haku tuoteryhmällä, jos se on asetettu käyttöliittymässä <-- seuraavat haut tarkentavat tätä tulosjoukkoa
+            if (!String.IsNullOrEmpty(ProductCategory) && (ProductCategory != "0"))
+            {
+                int para = int.Parse(ProductCategory);
+                tuotteet = tuotteet.Where(p => p.CategoryID == para);
+            }
+
+            if (!String.IsNullOrEmpty(searchString1)) //Jos hakufiltteri on käytössä, niin käytetään sitä ja sen lisäksi lajitellaan tulokset 
+            {
+                switch (sortOrder)
+                {
+                    case "productname_desc":
+                        tuotteet = tuotteet.Where(p => p.ProductName.Contains(searchString1)).OrderByDescending(p => p.ProductName);
+                        break;
+                    case "UnitPrice":
+                        tuotteet = tuotteet.Where(p => p.ProductName.Contains(searchString1)).OrderBy(p => p.UnitPrice);
+                        break;
+                    case "UnitPrice_desc":
+                        tuotteet = tuotteet.Where(p => p.ProductName.Contains(searchString1)).OrderByDescending(p => p.UnitPrice);
+                        break;
+                    default:
+                        tuotteet = tuotteet.Where(p => p.ProductName.Contains(searchString1)).OrderBy(p => p.ProductName);
+                        break;
+                }
+            }
+            else if (!String.IsNullOrEmpty(ProductCategory) && (ProductCategory != "0")) //Jos käytössä on tuoteryhmärajaus, niin käytetään sitä ja sen lisäksi lajitellaan tulokset 
+            {
+                int para = int.Parse(ProductCategory);
+                switch (sortOrder)
+                {
+                    case "productname_desc":
+                        tuotteet = tuotteet.Where(p => p.CategoryID == para).OrderByDescending(p => p.ProductName);
+                        break;
+                    case "UnitPrice":
+                        tuotteet = tuotteet.Where(p => p.CategoryID == para).OrderBy(p => p.UnitPrice);
+                        break;
+                    case "UnitPrice_desc":
+                        tuotteet = tuotteet.Where(p => p.CategoryID == para).OrderByDescending(p => p.UnitPrice);
+                        break;
+                    default:
+                        tuotteet = tuotteet.Where(p => p.CategoryID == para).OrderBy(p => p.ProductName);
+                        break;
+                }
+            } else { //Tässä hakufiltteri EI OLE käytössä, joten lajitellaan koko tulosjoukko ilman suodatuksia 
+                switch (sortOrder)
+                {
+                    case "productname_desc":
+                        tuotteet = tuotteet.OrderByDescending(p => p.ProductName);
+                        break;
+                    case "UnitPrice":
+                        tuotteet = tuotteet.OrderBy(p => p.UnitPrice);
+                        break;
+                    case "UnitPrice_desc":
+                        tuotteet = tuotteet.OrderByDescending(p => p.UnitPrice);
+                        break;
+                    default:
+                        tuotteet = tuotteet.OrderBy(p => p.ProductName);
+                        break;
+                }
+            };
+
+            List<Categories> lstCategories = new List<Categories>();
+
+            var categoryList = from cat in db.Categories
+                               select cat;
+
+            Categories tyhjaCategory = new Categories();
+            tyhjaCategory.CategoryID = 0;
+            tyhjaCategory.CategoryName = "";
+            tyhjaCategory.CategoryIDCategoryName = "";
+            lstCategories.Add(tyhjaCategory);
+
+            foreach (Categories category in categoryList)
+            {
+                Categories yksiCategory = new Categories();
+                yksiCategory.CategoryID = category.CategoryID;
+                yksiCategory.CategoryName = category.CategoryName;
+                yksiCategory.CategoryIDCategoryName = category.CategoryID.ToString() + " - " + category.CategoryName;
+                //Taulun luokkamääritykseen Models-kansiossa piti lisätä tämä "uusi" kenttä = CategoryIDCategoryName
+                lstCategories.Add(yksiCategory);
+            }
+            ViewBag.CategoryID = new SelectList(lstCategories, "CategoryID", "CategoryIDCategoryName", ProductCategory);
+
+            int pageSize = (pagesize ?? 10); //Tämä palauttaa sivukoon taikka jos pagesize on null, niin palauttaa koon 10 riviä per sivu
+            int pageNumber = (page ?? 1); //Tämä palauttaa sivunumeron taikka jos page on null, niin palauttaa numeron yksi
+            return View(tuotteet.ToPagedList(pageNumber, pageSize));
+        }
+
+        public ActionResult ProdCards()
+        {
+            if (Session["UserName"] == null)
+            {
+                return RedirectToAction("MustAuthorize", "home");
+            }
+            else
+            {
+                northwindEntities db = new northwindEntities();
+                List<Products> tuotteet = db.Products.ToList();
+                db.Dispose();
+                return View(tuotteet);
+            }
+
+        }
+
+    }
+}
